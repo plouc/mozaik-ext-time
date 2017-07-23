764c8ed3c60b0f566eafd4c591a3a64f158deb95
Raphaël Benitte <raphael.benitte@fullsix.com>
2017-07-23 10:33:52 +0900

    feat(themes): split theme registration

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './register_themes'
 import './register_extensions'
-import Mozaik, { ThemeManager } from '@mozaik/ui'
-
-// themes
-import {
-    miniKuroTheme,
-    nightBlueTheme,
-    snowTheme,
-    solarizedDarkTheme,
-    wineTheme,
-} from '@mozaik/themes'
-
-ThemeManager.add(miniKuroTheme)
-ThemeManager.add(nightBlueTheme)
-ThemeManager.add(snowTheme)
-ThemeManager.add(solarizedDarkTheme)
-ThemeManager.add(wineTheme)
-
-ThemeManager.defaultTheme = solarizedDarkTheme.name
+import Mozaik from '@mozaik/ui'
 
 ReactDOM.render(<Mozaik />, document.getElementById('root'))
